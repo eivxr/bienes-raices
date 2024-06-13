6268504910930d5841654a37662e5fa004322fe9
Eivar <putyfive@gmail.com>
2024-06-13 11:22:26 -0700

    FIX: db connection port

--- config/db.js
+++ config/db.js
@@ -1,21 +1,26 @@
 import { Sequelize } from "sequelize";
-import dotenv from 'dotenv';
-dotenv.config({path:'.env'})
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
 
-const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS ?? '',{
-    host:process.env.BD_HOST,
-    port:'3306',
-    dialect:'mysql',
-    define:{
-        timestamps:true
+const db = new Sequelize(
+  process.env.BD_NOMBRE,
+  process.env.BD_USER,
+  process.env.BD_PASS ?? "",
+  {
+    host: process.env.BD_HOST,
+    port: 3307,
+    dialect: "mysql",
+    define: {
+      timestamps: true,
     },
-    poll:{
-        max:5, //maximo de conexiones a retulizar
-        min:0, //minimo
-        acquire:30000, //tiempo antes de error
-        idle:10000 //tiempo para eliminar conexiones
+    poll: {
+      max: 5, //maximo de conexiones a retulizar
+      min: 0, //minimo
+      acquire: 30000, //tiempo antes de error
+      idle: 10000, //tiempo para eliminar conexiones
     },
-    operatorsAliases:false
-});
+    operatorsAliases: false,
+  }
+);
 
 export default db;
